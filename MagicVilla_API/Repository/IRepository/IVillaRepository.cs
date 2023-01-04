8fd22e8dba588e00d2f90c1413e88402779a4be9
Gaorieh Isaac <gisaac@letsbuildit.nl>
2023-01-04 13:39:49 +0100

    Repository cleanup - make it generic

--- MagicVilla_API/Repository/IRepository/IVillaRepository.cs
+++ MagicVilla_API/Repository/IRepository/IVillaRepository.cs
@@ -3,14 +3,8 @@ using System.Linq.Expressions;
 
 namespace MagicVilla_API.Repository.IRepository
 {
-    public interface IVillaRepository
-    {
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa,bool>> filter = null);
-        Task<Villa> GetAsync(Expression<Func<Villa,bool>> filter = null, bool tracked = true);
-        Task CreateAsync(Villa entity);
-        Task UpdateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-        Task SaveAsync();
-
+    public interface IVillaRepository : IRepository<Villa>
+    {      
+        Task<Villa> UpdateAsync(Villa entity);      
     }
 }
